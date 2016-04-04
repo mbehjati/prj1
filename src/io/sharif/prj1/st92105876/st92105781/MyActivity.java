23c001879fb2e7fdfe6fbcea7720ca991fbf84af
mina-rf <rafiei.mina73@gmail.com>
2016-04-04 20:39:48 +0430

    Show menus function added

--- src/io/sharif/prj1/st92105876/st92105781/MyActivity.java
+++ src/io/sharif/prj1/st92105876/st92105781/MyActivity.java
@@ -2,6 +2,11 @@ package io.sharif.prj1.st92105876.st92105781;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +16,41 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void showGameMenu (View v) {
+
+        PopupMenu gameMenuPopup =  new PopupMenu(MyActivity.this, v);
+        gameMenuPopup.getMenuInflater().inflate(R.menu.menu1, gameMenuPopup.getMenu());
+//        gameMenuPopup.setOnMenuItemClickListener(this);
+//        gameMenuPopup.inflate(R.menu.menu1);
+        gameMenuPopup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d("menu","clicked");
+                Toast.makeText(MyActivity.this,"You Clicked : "+item.getTitle(),Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        gameMenuPopup.show();
+
+    }
+
+    public void showAboutusMenu (View v) {
+
+        PopupMenu aboutusMenuPopup =  new PopupMenu(MyActivity.this, v);
+        aboutusMenuPopup.getMenuInflater().inflate(R.menu.menu2, aboutusMenuPopup.getMenu());
+//        aboutusMenuPopup.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+//
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//
+//
+//                return true;
+//            }
+//        });
+
+        aboutusMenuPopup.show();
+
     }
 }
